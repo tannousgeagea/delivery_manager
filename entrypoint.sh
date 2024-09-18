4747d6faedde3fbc8e726225d57db29673fca6e5
tannousgeagea <tannous.geagea@wasteant.com>
2024-09-18 18:39:28 +0200

    fixed path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-/bin/bash -c "python3 /home/$user/src/external_viz_manager/manage.py makemigrations"
-/bin/bash -c "python3 /home/$user/src/external_viz_manager/manage.py migrate"
-/bin/bash -c "python3 /home/$user/src/external_viz_manager/manage.py create_superuser"
+/bin/bash -c "python3 /home/$user/src/delivery_manager/manage.py makemigrations"
+/bin/bash -c "python3 /home/$user/src/delivery_manager/manage.py migrate"
+/bin/bash -c "python3 /home/$user/src/delivery_manager/manage.py create_superuser"
 
 # Start Supervisor
 sudo -E supervisord -n -c /etc/supervisord.conf
